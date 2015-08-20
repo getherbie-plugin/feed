adae3de7705ac6bed2e32a5537f1986134cc683b
Thomas Breuss <thomasbreuss@gmx.ch>
2015-08-20 21:24:06 +0200

    Merge branch 'rework'

--- FeedPlugin.php
+++ FeedPlugin.php
@@ -12,21 +12,11 @@
 namespace herbie\plugin\feed;
 
 use Herbie;
-use Herbie\Loader\FrontMatterLoader;
-use Herbie\Menu\Page\Item;
 
 class FeedPlugin extends Herbie\Plugin
 {
 
-    /**
-     * @var Herbie\Menu\Page
-     */
-    private $menu;
-
-    /**
-     * @param Herbie\Event $event
-     */
-    public function onPluginsInitialized(Herbie\Event $event)
+    public function onPluginsInitialized()
     {
         if($this->config->isEmpty('plugins.config.feed.no_page')) {
             $this->config->push('pages.extra_paths', '@plugin/feed/pages');
